5bab589c6a34636d98df88fcfc82d19db75e261b
Russell Savage <russell.savage@elasticsearch.com>
2015-05-18 15:29:18 -0700

    fixing gemspec

--- spec/outputs/salesforce_spec.rb
+++ spec/outputs/salesforce_spec.rb
@@ -1,20 +1,8 @@
+# encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/outputs/salesforce"
 require "logstash/event"
 
 describe LogStash::Outputs::SalesForce do
-  let(:sample_event) { LogStash::Event.new }
-  let(:output) { LogStash::Outputs::Example.new }
-
-  before do
-    output.register
-  end
-
-  describe "receive message" do
-    subject { output.receive(sample_event) }
-
-    it "returns a string" do
-      expect(subject).to eq("Event received")
-    end
-  end
+  # I should figure out how to write these
 end
